63c266b91f8eddb88b784a5ab0cf908090579f20
Thomas Jolley <tomjolley81@gmail.com>
2018-11-19 20:58:04 -0800

    Going back to work

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -17,10 +17,28 @@ var view = {
     cell.setAttribute("class", "miss");
   }
 };
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("Tap, tap, is this thing on?");
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] }
+  ],
+
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var locations = ship.locations.indexOf(guess);
+      if (index >= 0) {
+        //We have a hit
+        ship.hits[index] = "hit";
+        return true;
+      }
+    }
+    return false;
+  }
+};
